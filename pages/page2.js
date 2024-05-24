98fd9472dbac6dd09a4386c392364bbc0c8b8b7a
lasteryas <43411435+lasteryas@users.noreply.github.com>
2024-05-24 00:12:42 -0400

    Update and rename page2.html to page2.js

--- pages/page2.js
+++ pages/page2.js
@@ -1,4 +1,7 @@
-<div class="partie-one">
+import Head from 'next/head'
+import Header from '@components/Header'
+export default function Home() {
+  return (<div class="partie-one">
 <img class="imgdefiche" src="https://images2.imgbox.com/85/25/ao2BRJ4h_o.jpg"><div class="sous-partie-un">
 <div class="infos_fiche">
 Hey, Yash.
@@ -8,3 +11,5 @@ Happy Birthday! </div>
 </div>
 
 <div class="story_fiche"><div class="fiche_titre"></div></div>
+)
+}
