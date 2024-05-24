72477ab61f5a6910bc0cbdcf92bc242d2a1d1ebb
lasteryas <43411435+lasteryas@users.noreply.github.com>
2024-05-23 23:47:17 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,19 @@ import Head from 'next/head'
 import Header from '@components/Header'
 export default function Home() {
   return (
-    <div className="container">
+   <body> <div className="container">
       <Head>
         <title>Hey, Yash.</title>
       </Head>
-
       <main>
-      <script src="https://run.confettipage.com/here.js" data-confetticode="U2FsdGVkX1/Uar+zOtMPNF+hnuxkU4ZmRZuD4gZFnM42ZOViVSniwjn3fwJn6vO+9F5xw19+Fk+33MD48MdZyrwZhrKF3YtI0GnBY/KxTiM+Dx+L9MeLWQwolx6R43CKPcyO3L3a7rQnpXsLFnRQVJEKkuBWo1cJgAdnVHnm2p0JKhzFURU6K+JSwBFMKADMk4UKv7welBGFhN2phul6d4yVnJrYxLqbdXXl7zIoWAaHkpvrTOQdpWtgVJsuOszCkmVdcpUh6nqc9DlhKT18VS731bHQjRz+mmuDVzm2HaMn7NBhiDv/XbJ+6FvDAdAlDbxaeodQOGMGk2SddAxihz4CAQbkk+znigCdVn9zdK3vWQ3xQb9ZVBTE86fN11F449MaEyrb6aEoH424/To2bsmN0Znxq8BeMPQXqICTv8ScINQAOeAWlnrt/2aU9KYsSANO/NgYnz/0d7Dc0U5H/BiuXxXJVl04RpE5p+1dCaUVod6KVCWlwMxh8rj3ST8/bRtcj23C/0GLlmWD75jryiXzOd6bqhbCNJyAhCJYJsMJ8aF0OqR5WzpoxddyU+O+Wgm/JVs4sI/8ZzaEkKFsRyKvKcPaTgB3i4vv91i3TCy9proYzmi8ctnZShXPW/qWWlt/JggbiVbFnZRyfh/LskPTlpY1fWnfuReGP/lq8vSH00a05sz3AepNNmtsFVTg"></script>
+      
         <Header title="Happy Birthday!" />
         <p className="description">
           Someone's turning 30. LOL.
         </p>
       </main>
     </div>
+    <script src="https://run.confettipage.com/here.js" data-confetticode="U2FsdGVkX18sNxmzGqwtH0v6+tL7fhEX55Gm9X9kfoNr6gkvRcQFS7bcp4eyRsQG7aoC1Ikn0zruaoDCi2qoY8YMQjid63LhZpLi6fJh2TMlr63oOwkOcY95T9LsdvkFJSz9bc2/+zsRHkakNxOgqb5mxdRx4JXElZoM+dr/h0iEP5CIWINP38a8lp7PX/qHSGDQ9aW15QctNmhkjzAlFr4tbQSYrAlnkPIMqGz8SJWNkDXhqXiiIVz5RHkO90kqqzJWo2M9+1mqkxQCwYHBbOmrgwWF2Z98U6niZjJzyv9wRPQ6trO10NsuDT1iN85vgk1Cnmc0tUkjOgmqS+ekiCAWhptSVL6rjtffmY3VV+0ljOzHOp+WyAkLWPdD6GGYsDd1ePalFVfHcJpoqrYPgwb6zk+hju5HVLYM6fwavlmiiLOKN2nACStLWo5VJw6UNY9iW/JDDY5BLE+k8brOAQ82eEuFjA9bRzXE3BAHZWGxLk0Cn++ZFb/R9b10j2J/qrz0SXP5iOfUykKLEfJXOB6yYvCdaBLES5zPK0PawqbrkfPnpHrcvT1xsCEyC6BdgnhgANhkqbwLweSdfStmmMOXuG6qywl51tYsJSy95k/R/XJNcy1XgDTHYEUmfClcLufGDaSjNm0jt3NOP6PDqXiusOMUbIEZfnoEDnYBqiWNP7wWFOTT5CtC5lGiaohS"></script>
+    </body>
   )
 }
